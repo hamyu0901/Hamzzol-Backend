4c128b670f85cef8ab4e7a439d38748712766965
yujin <yujin@doolim-yaskawa.com>
2023-08-25 17:47:29 +0900

    [HZBack-2] /routes/login/login.service.ts

--- src/routes/login/login.service.ts
+++ src/routes/login/login.service.ts
@@ -1,11 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { LoginRepository } from '@/routes/login/login.repository';
+import { isEmpty } from 'lodash';
 
 @Injectable()
 export class LoginService {
   constructor(private readonly loginRepository: LoginRepository) {}
   async getUserInfo(): Promise<any> {
     return await this.loginRepository.getUserInfo();
+  }
+  async getCompareUserInfo(params: {
+    id: string;
+    password: string;
+  }): Promise<any> {
+    const userInfo = await this.loginRepository.getUserInfo();
+    if (!isEmpty(userInfo)) {
+      console.log(params)
+    }
+    console.log('usrInfoIII', userInfo);
   }
   async getSecurityQATypeInfo(): Promise<any> {
     return await this.loginRepository.getSecurityQATypeInfo();
