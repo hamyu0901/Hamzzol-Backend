2c0134049b805923c9420ee572978df53694da19
yujin <yujin@doolim-yaskawa.com>
2023-09-06 17:31:56 +0900

    [HZBack-7] /routes/common/common.service.ts  - 불필요 코드 수정

--- src/routes/common/common.service.ts
+++ src/routes/common/common.service.ts
@@ -1,10 +1,8 @@
 import { Injectable } from '@nestjs/common';
-
+const weatherApiKey = '71878480cdd4f838da85f3940b699b86&units=metric';
 @Injectable()
 export class CommonService {
-  private readonly;
   async getWeatherAPI(): Promise<string> {
-    const weatherApiKey = '71878480cdd4f838da85f3940b699b86&units=metric';
     return weatherApiKey;
   }
 }
