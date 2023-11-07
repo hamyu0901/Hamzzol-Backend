1b5c6b4cef036afe495e77ecd2451d77de86fdfb
yujin <yujin@doolim-yaskawa.com>
2023-11-07 17:44:55 +0900

    [HZBack-5] /planner.service.ts  - patchPlannerList  - postPlannerList  - deletePlannerList

--- src/routes/planner/planner.service.ts
+++ src/routes/planner/planner.service.ts
@@ -12,10 +12,22 @@ export class PlannerService {
   }): Promise<any> {
     return await this.plannerRepository.getPlannerList(query);
   }
-  async patchPlannerList(
-    body: IPostTodoListItemType,
-    id: number,
-  ): Promise<any> {
-    return await this.plannerRepository.patchPlannerList(body, id);
+  async patchPlannerList(body: IPostTodoListItemType, id: number) {
+    await Promise.all([
+      this.plannerRepository.patchPlannerList(body, id),
+      this.plannerRepository.patchSubPlannerList(body, id),
+    ]);
+  }
+  async postPlannerList(body: IPostTodoListItemType) {
+    const mainResult = await this.plannerRepository.postMainPlannerList(body);
+    const listId = mainResult[0].list_id;
+    await this.plannerRepository.postSubPlannerList(body, listId);
+  }
+
+  async deletePlannerList(listId: number) {
+    await Promise.all([
+      this.plannerRepository.deletePlannerList(listId),
+      this.plannerRepository.deleteSubPlannerList(listId),
+    ]);
   }
 }
